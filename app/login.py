f4f966be5f55536ed85f731a78aaac8d3430449f
Dru-Crack22 <84838589+Dru-Crack22@users.noreply.github.com>
2021-06-16 03:38:45 +0700

    Add files via upload

---
+++ app/login.py
@@ -0,0 +1,36 @@
+#!usr/bin/python2.7
+# coding=utf-8
+
+#######################################################
+# Name           : Multi BF (MBF) <cookie method>     #
+# File           : login.py                           #
+# Author         : DulLah                             #
+# Github         : https://github.com/dz-id           #
+# Facebook       : https://www.facebook.com/dulahz    #
+# Telegram       : https://t.me/unikers               #
+# Python version : 2.7                                #
+#######################################################
+
+import os, time
+from src import language
+
+def loginFb(self, url, config):
+	os.system('clear')
+	print(config.banner())
+	print('\n[©] Login Pakai FB cookie Kalian : [©]\n')
+	while True:
+		cookies = raw_input('Masukan Cookie Facebook Kalian: ')
+		response = config.httpRequest(url, cookies).encode('utf-8')
+		if 'mbasic_logout_button' in str(response):
+			print('\nTunggu Beberapa Detik...')
+			language.main(cookies, url, config)
+			try: os.mkdir('log')
+			except: pass
+			save = open('log/cookies.log','w')
+			save.write(cookies.strip())
+			save.close()
+			print('\n\033[0;92mLogin Berhasil✓\033[0m')
+			time.sleep(2)
+			break
+		else:
+			print('\n\033[0;91mCookie Salah, Coba Lagi.\n\033[0m')

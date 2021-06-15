9ab1c8f03e03e2b9c63e5ad692c86cfdc198413f
Dru-Crack22 <84838589+Dru-Crack22@users.noreply.github.com>
2021-06-16 04:19:30 +0700

    Update login.py

--- app/login.py
+++ app/login.py
@@ -3,7 +3,7 @@
 
 #############################################################
 # Name           : Multi BF (MBF) <cookie method>           #
-# File           : Dru.py                                   #
+# File           : login.py                                 #
 # Author         : Badru                                    #
 # Github         : https://github.com/Dru-Crack22           #
 # Facebook       : https://www.facebook.com/Bang.badru23    #
